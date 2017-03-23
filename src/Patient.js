4528d93a5b1d99c662535626dbc13a3d2e246acf
tostaylo <tostaylo1@gmail.com>
2017-03-22 18:47:43 -0700

    Recording Start Stop Functionality
    Added the ability to start and stop recording on proper screens

--- src/Patient.js
+++ src/Patient.js
@@ -1,4 +1,10 @@
 import React, {Component} from 'react';
+import RecordRTC from 'recordrtc';
+import {BASE_URL} from './helpers.js';
+import axios from 'axios';
+var Whammy = RecordRTC.Whammy;
+var video = new Whammy.Video(100);
+var recordRTC;
 
 class PatientHome extends Component {
 
@@ -6,17 +12,65 @@ class PatientHome extends Component {
     super(props)
     this.state = {
       idx: 0,
-      videoIdx:1
+      videoIdx:1,
+	    recordVideo: null,
+	    src: null,
+	    uploadSuccess: null,
+	    uploading: false
     }
     this.handleSpaceBar = this.handleSpaceBar.bind(this)
+	  this.startRecord = this.startRecord.bind(this);
+	  this.stopRecord = this.stopRecord.bind(this);
+  }
+
+	captureUserMedia(callback) {
+		let params = {audio: true, video: true};
+		navigator.getUserMedia(params, callback, (error) => {
+			alert(JSON.stringify(error));
+		});
+	};
+
+	startRecord() {
+		this.captureUserMedia((mediaStream) => {
+			var options = {
+				mimeType: 'video/webm',
+				audioBitsPerSecond: 128000,
+				videoBitsPerSecond: 128000
+			};
+			recordRTC = RecordRTC(mediaStream, options)
+			recordRTC.startRecording()
+		});
+		setTimeout(() => {
+			this.stopRecord();
+		}, 4000);
+	}
+
+	stopRecord() {
+		let recordedBlob;
+		recordRTC.stopRecording(function(){
+			let fd = new FormData();
+			recordedBlob = recordRTC.getBlob();
+			fd.append('fname', 'video_' + Date.now() + '.webm');
+			fd.append('file', recordedBlob)
+			axios.post(`${BASE_URL}/api/recording`, fd).then(response => {
+			})
+		});
+	}
+
+	componentDidMount(){
+		//this.startRecord()
   }
 
   handleSpaceBar(event) {
     if (event.keyCode === 32) {
       event.preventDefault()
       if(this.state.videoIdx <=  this.props.videosLength){
+        if(this.state.idx === 2){
+          this.startRecord()
+        }
         if(this.state.idx === 5 && this.state.videoIdx === this.props.videosLength){
           this.setState({idx: 6})
+          this.stopRecord()
         }else if(this.state.idx <= 4){
           this.setState({idx: ++this.state.idx})
         } else if (this.state.idx === 5 && this.state.videoIdx !== this.props.videosLength){
@@ -31,7 +85,7 @@ class PatientHome extends Component {
     return (
       <div>
         <h1>Welcome Patient</h1>
-        <div>{this.props.children[this.state.idx]}</div>
+      <div>{this.props.children[this.state.idx]}</div>
       </div>
     )
   }
--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,68 +1,18 @@
 import React, {Component} from 'react';
 import YouTube from 'react-youtube';
-import RecordRTC from 'recordrtc';
-import {BASE_URL} from './helpers.js';
-import axios from 'axios';
-var Whammy = RecordRTC.Whammy;
-var video = new Whammy.Video(100);
-var recordRTC;
 
 class VideoPlayer extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			recordVideo: null,
-			src: null,
-			uploadSuccess: null,
-			uploading: false
-		};
-		this.startRecord = this.startRecord.bind(this);
-		this.stopRecord = this.stopRecord.bind(this);
+
+		}
 	}
 
 	_onReady(event) {
 		// access to player in all event handlers via event.target
 		event.target.playVideo();
 	}
-
-	captureUserMedia(callback) {
-		let params = {audio: true, video: true};
-		navigator.getUserMedia(params, callback, (error) => {
-			alert(JSON.stringify(error));
-		});
-	};
-
-	startRecord() {
-		this.captureUserMedia((mediaStream) => {
-			var options = {
-				mimeType: 'video/webm',
-				audioBitsPerSecond: 128000,
-				videoBitsPerSecond: 128000
-			};
-			recordRTC = RecordRTC(mediaStream, options)
-			recordRTC.startRecording()
-		});
-		setTimeout(() => {
-			this.stopRecord();
-		}, 4000);
-	}
-
-	stopRecord() {
-		let recordedBlob;
-		recordRTC.stopRecording(function(){
-			let fd = new FormData();
-			recordedBlob = recordRTC.getBlob();
-			fd.append('fname', 'video_' + Date.now() + '.webm');
-			fd.append('file', recordedBlob)
-			axios.post(`${BASE_URL}/api/recording`, fd).then(response => {
-			})
-		});
-	}
-
-	componentDidMount(){
-		//this.startRecord()
-	}
-
 	render() {
 		const opts = {
 			height: '390',
@@ -72,11 +22,13 @@ class VideoPlayer extends Component {
 			}
 		};
 		return (
-			<YouTube
-				videoId={this.props.video.youtube_id}
-				opts={opts}
-				onReady={this._onReady}
-			/>
+			<div>
+				<YouTube
+					videoId={this.props.video.youtube_id}
+					opts={opts}
+					onReady={this._onReady}
+				/>
+			</div>
 		)
 	}
 }

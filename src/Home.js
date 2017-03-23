1093361cb5a4e6284d29b73e9a81924ee266560e
Andrew Mundy <andrewmundy@gmail.com>
2017-03-23 15:50:56 -0700

    Fixed bottom div for good! Updated onblur to remove email form 💆

--- src/Home.js
+++ src/Home.js
@@ -17,7 +17,6 @@ class Home extends Component {
           <img className="content-image" alt="UCSF" src={ucsf}/>
           <p className="content-logo body-bold serif">CAMS was developed by UCSF as a way to dynamically create personalized baseline mental health exams. For a more rapid diagnosis and a far exeptional treatment.</p>
         </div>
-        <div className="diagonalbottom"></div>
       </div>
     )
   }

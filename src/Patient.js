deed9bb7307d28ef1637e1d91ad59eef6b0efa09
tostaylo <tostaylo1@gmail.com>
2017-03-23 09:53:10 -0700

    Commented on handlespaceBar in patient.js
    fixed indentations

--- src/Patient.js
+++ src/Patient.js
@@ -2,26 +2,26 @@ import React, {Component} from 'react';
 import RecordRTC from 'recordrtc';
 import {BASE_URL} from './helpers.js';
 import axios from 'axios';
-var Whammy = RecordRTC.Whammy;
-var video = new Whammy.Video(100);
-var recordRTC;
+let Whammy = RecordRTC.Whammy;
+let video = new Whammy.Video(100);
+let recordRTC;
 
 class PatientHome extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      idx: 0,
-      videoIdx:1,
-	    recordVideo: null,
-	    src: null,
-	    uploadSuccess: null,
-	    uploading: false
-    }
-    this.handleSpaceBar = this.handleSpaceBar.bind(this)
-	  this.startRecord = this.startRecord.bind(this);
-	  this.stopRecord = this.stopRecord.bind(this);
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			idx: 0,
+			videoIdx: 1,
+			recordVideo: null,
+			src: null,
+			uploadSuccess: null,
+			uploading: false
+		}
+		this.handleSpaceBar = this.handleSpaceBar.bind(this)
+		this.startRecord = this.startRecord.bind(this);
+		this.stopRecord = this.stopRecord.bind(this);
+	}
 
 	captureUserMedia(callback) {
 		let params = {audio: true, video: true};
@@ -40,51 +40,57 @@ class PatientHome extends Component {
 			recordRTC = RecordRTC(mediaStream, options)
 			recordRTC.startRecording()
 		});
-		setTimeout(() => {
-			this.stopRecord();
-		}, 4000);
 	}
 
 	stopRecord() {
 		let recordedBlob;
-		recordRTC.stopRecording(function(){
+		recordRTC.stopRecording(function () {
 			let fd = new FormData();
 			recordedBlob = recordRTC.getBlob();
 			fd.append('fname', 'video_' + Date.now() + '.webm');
 			fd.append('file', recordedBlob)
 			axios.post(`${BASE_URL}/api/recording`, fd).then(response => {
+			}).catch(err => {
+				console.log(err)
 			})
 		});
 	}
 
-  handleSpaceBar(event) {
-    if (event.keyCode === 32) {
-      event.preventDefault()
-      if(this.state.videoIdx <=  this.props.videosLength){
-        if(this.state.idx === 2){
-          this.startRecord()
-        }
-        if(this.state.idx === 5 && this.state.videoIdx === this.props.videosLength){
-          this.setState({idx: 6})
-          this.stopRecord()
-        }else if(this.state.idx <= 4){
-          this.setState({idx: ++this.state.idx})
-        } else if (this.state.idx === 5 && this.state.videoIdx !== this.props.videosLength){
-          this.setState({idx: 2, videoIdx: ++this.state.videoIdx})
-        }
-      }
-    }
-  }
+	handleSpaceBar(event) {
+		if (event.keyCode === 32) {
+			event.preventDefault()
+			// If the current video index is less than the length of the playlist
+			if (this.state.videoIdx <= this.props.videosLength) {
+				//If the first two instructional videos have completed and the video component has rendered start the recording
+				if (this.state.idx === 2) {
+					this.startRecord()
+				}
+				//If the timer for the question response has completed and we have made it through all the videos in the
+				//stop recording and send the recording file
+				if (this.state.idx === 5 && this.state.videoIdx === this.props.videosLength) {
+					this.setState({idx: 6})
+					this.stopRecord()
+					// Keep cycling through the components until we reach the timer
+				} else if (this.state.idx <= 4) {
+					this.setState({idx: ++this.state.idx})
+					//If we reach the timer and we are not on the last video in the playlist then go to the next video in the
+					//playlist's instructions.
+				} else if (this.state.idx === 5 && this.state.videoIdx !== this.props.videosLength) {
+					this.setState({idx: 2, videoIdx: ++this.state.videoIdx})
+				}
+			}
+		}
+	}
 
-  render() {
-    document.addEventListener("keydown", this.handleSpaceBar)
-    return (
-      <div>
-        <h1>Welcome Patient</h1>
-      <div>{this.props.children[this.state.idx]}</div>
-      </div>
-    )
-  }
+	render() {
+		document.addEventListener("keydown", this.handleSpaceBar)
+		return (
+			<div>
+				<h1>Welcome Patient</h1>
+				<div>{this.props.children[this.state.idx]}</div>
+			</div>
+		)
+	}
 }
 
 export default PatientHome;

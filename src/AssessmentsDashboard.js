1cf7bb67afdad345fe70877c3b5191530a1b9e58
Joel <japoncea@gmail.com>
2017-03-24 10:23:27 -0700

    fixing email form bug (fixes #23)

---
+++ src/AssessmentsDashboard.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+// import {BASE_URL} from './helpers.js';
+// import axios from 'axios';
+import {Link} from 'react-router-dom';
+
+class Assessment extends Component {
+
+  render(){
+    return(
+      <div>
+        <div className="banner-text">
+          <h1 className="banner-bold">Patient Assessments</h1>
+        </div>
+        <div className="content">
+          <div className="dash-nav">
+            <Link to="/playlists/new">
+              <button
+                className="button button-hover big-nav"
+                type="submit"
+                value="Submit">
+                New Playlist
+              </button>
+            </Link>
+            <Link to="/assessments">
+              <button
+                className="button button-hover big-nav"
+                type="submit"
+                value="Submit">
+                Assessments
+              </button>
+            </Link>
+          </div>
+          <div className="playlist-card">
+
+            <button
+              className="button button-hover"
+              value="info">
+              View Details
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Assessment;

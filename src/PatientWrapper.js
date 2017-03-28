35cc10496e5f7cd585a34e67360027c8abdca24f
James Robinson II <jrob286@gmail.com>
2017-03-28 10:42:43 -0700

    Added text to the video playlist

--- src/PatientWrapper.js
+++ src/PatientWrapper.js
@@ -17,7 +17,9 @@ class PatientWrapper extends Component {
 
   componentWillMount(){
     var self = this;
-    axios.get(`${BASE_URL}/api/assessments/21`).then(function (response) {
+    let userId = JSON.parse(atob(localStorage.getItem('token').split('.')[1])).id
+
+    axios.get(`${BASE_URL}/api/users/2/assessments/1`).then(function (response) {
       self.setState({videoIds: response.data.screens});
     })
   }
@@ -25,11 +27,11 @@ class PatientWrapper extends Component {
   render() {
     return(
       <PatientHome videosLength={this.state.videoIds.length}>
-        <p>this is the first instructional message Press Space Bar to Continue</p>
-        <p>this is the second instructional message Press Space Bar to Continue</p>
-        <p>Please Watch the Video</p>
+        <p>Welcome! You will watch short videos and answer questions about yourself. <br />Please read the instructions carefully. You will have 30 seconds to respond to each question. <br />Your answers will be recorded so please speak OUT LOUD when responding. <br /><br />Please press the spacebar when you are ready to go forward</p>
+        <p>You will now watch several short video clips. <br />After each video, you will be asked questions  about what happened in the video. <br /> You will have 30 secons  to answer each question. <br />Please speak out loud and keep speaking until the time runs out. <br /><br />When you are ready, please press the spacebar to continue.</p>
+        <p>When you are ready, please press the spacebar to continue to the video.</p>
         <VideoPlayer videos={this.state.videoIds} />
-        <p>Answer the Question</p>
+        <p>Please describe what happened in the video. <br /> Use as much detail as possible when describing the video. <br />You have 30 seconds. Please try to talk for the entire duration of the timer.<br /><br />Please press the spacebar when you are ready to start.</p>
         <p>Timer</p>
         <p>Bye!</p>
       </PatientHome>

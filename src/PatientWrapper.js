842abfbd06aa3f84b7ee329ca15177ba2683e234
tbobarino <hopmailkins@gmail.com>
2017-03-29 13:43:57 -0700

    added Text component to Patient Wrapper to replace p tags

--- src/PatientWrapper.js
+++ src/PatientWrapper.js
@@ -4,6 +4,8 @@ import VideoPlayer from './VideoPlayer'
 import {BASE_URL, userID} from './helpers.js';
 import axios from 'axios';
 
+const Text = props => <p>{props.children}</p>
+
 class PatientWrapper extends Component {
   constructor(props) {
     super(props)
@@ -25,13 +27,13 @@ class PatientWrapper extends Component {
   render() {
     return(
       <PatientHome videosLength={this.state.videoIDs.length}>
-        <p>Welcome! You will watch short videos and answer questions about yourself. <br />Please read the instructions carefully. You will have 30 seconds to respond to each question. <br />Your answers will be recorded so please speak OUT LOUD when responding. <br /><br />Please press the spacebar when you are ready to go forward</p>
-        <p>You will now watch several short video clips. <br />After each video, you will be asked questions  about what happened in the video. <br />You will have 30 secons  to answer each question. <br />Please speak out loud and keep speaking until the time runs out. <br /><br />When you are ready, please press the spacebar to continue.</p>
-        <p>When you are ready, please press the spacebar to continue to the video.</p>
+        <Text>Welcome! You will watch short videos and answer questions about yourself. <br />Please read the instructions carefully. You will have 30 seconds to respond to each question. <br />Your answers will be recorded so please speak OUT LOUD when responding. <br /><br />Please press the spacebar when you are ready to go forward</Text>
+        <Text>You will now watch several short video clips. <br />After each video, you will be asked questions  about what happened in the video. <br />You will have 30 secons  to answer each question. <br />Please speak out loud and keep speaking until the time runs out. <br /><br />When you are ready, please press the spacebar to continue.</Text>
+        <Text>When you are ready, please press the spacebar to continue to the video.</Text>
         <VideoPlayer videos={this.state.videoIDs} />
-        <p>Please describe what happened in the video. <br />Use as much detail as possible when describing the video. <br />You have 30 seconds. Please try to talk for the entire duration of the timer.<br /><br />Please press the spacebar when you are ready to start.</p>
-        <p>Timer</p>
-        <p>Bye!</p>
+        <Text>Please describe what happened in the video. <br />Use as much detail as possible when describing the video. <br />You have 30 seconds. Please try to talk for the entire duration of the timer.<br /><br />Please press the spacebar when you are ready to start.</Text>
+        <Text>Timer</Text>
+        <Text>Bye!</Text>
       </PatientHome>
     )
   }

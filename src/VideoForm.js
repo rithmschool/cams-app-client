544fc3f37b4e0d4c8ac6a5eab177fb7feb3a41c7
aricWL <aricliesenfelt@gmail.com>
2017-03-30 12:36:42 -0700

    initial changes

--- src/VideoForm.js
+++ src/VideoForm.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+import {BASE_URL, userID, config} from './helpers.js';
 
 class VideoForm extends Component {
 
@@ -26,20 +28,39 @@ class VideoForm extends Component {
     this.props.addDone(this.state.url)
   }
 
+  componentWillMount(){
+    axios.get(
+      `${BASE_URL}/api/videos`, config
+    ).then(response => {
+      this.setState({videos: response.data})
+      console.log(this.state.videos)
+    })
+  }
+
   render(){
+    let videos = this.state.videos.map((val, idx, arr) => {
+      console.log(val)
+    })
+
     return(
       <div>
-        <div className="content">
-          <form onSubmit={this.handleAdd.bind(this)}>
-            <input type="url"
-              onChange={this.handleChange.bind(this)}
-              name="url"
-              placeholder="Video Url"
-              value={this.state.url}
-            />
-            <button type="submit" className="button" value="Add">+</button>
-            <button className="button" onClick={this.handleSubmit.bind(this)} value="Submit">Submit</button>
-          </form>
+        <div className="video-form-wrapper">
+          <div className="videos-list">
+            <h2>Videos</h2>
+
+          </div>
+          <div className="videos-form">
+            <form onSubmit={this.handleAdd.bind(this)}>
+              <input type="url"
+                onChange={this.handleChange.bind(this)}
+                name="url"
+                placeholder="Video Url"
+                value={this.state.url}
+              />
+              <button type="submit" className="button" value="Add">+</button>
+              <button className="button" onClick={this.handleSubmit.bind(this)} value="Submit">Submit</button>
+            </form>
+          </div>
         </div>
       </div>
     )

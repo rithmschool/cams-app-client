4bc5c173bda10ca8dbc0d265b7f576c58281983b
toChaim <toChaim@gmail.com>
2017-09-29 14:50:02 -0700

    add video viewer

--- src/AssessmentsDashboard.js
+++ src/AssessmentsDashboard.js
@@ -1,54 +1,70 @@
-import React, {Component} from 'react';
-import {BASE_URL, userID, config} from './helpers.js';
-import axios from 'axios';
+import React, { Component } from "react";
+import { BASE_URL, userID, config } from "./helpers.js";
+import axios from "axios";
 
 class AssessmentsDashboard extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       doctorAssessments: [],
       assessmentID: null,
       downloaded: []
-    }
+    };
   }
 
-  downloadFile(id){
-    axios.get(`${BASE_URL}/api/recording/${id}`, config())
-      .then(response => {
-        this.setState({downloaded: [response.data.id, response.data.url]})
-      })
+  downloadFile(id) {
+    axios.get(`${BASE_URL}/api/recording/${id}`, config()).then(response => {
+      this.setState({ downloaded: [response.data.id, response.data.url] });
+    });
   }
 
-  selectAssessment(assessmentID){
-    if (this.state.assessmentID === null || assessmentID !== this.state.assessmentID){
-      this.setState({assessmentID})
-    }
-    else if(this.state.assessmentID !== null){
-      this.setState({assessmentID: null})
+  selectAssessment(assessmentID) {
+    if (
+      this.state.assessmentID === null ||
+      assessmentID !== this.state.assessmentID
+    ) {
+      this.setState({ assessmentID });
+    } else if (this.state.assessmentID !== null) {
+      this.setState({ assessmentID: null });
     }
   }
 
   componentWillMount() {
-    axios.get(`${BASE_URL}/api/users/${userID()}/assessments`, config())
-    .then(response => {
-      this.setState({doctorAssessments: response.data})
-    })
+    axios
+      .get(`${BASE_URL}/api/users/${userID()}/assessments`, config())
+      .then(response => {
+        this.setState({ doctorAssessments: response.data });
+      });
   }
 
   render() {
     let assessments = this.state.doctorAssessments.map((assessment, i) => {
-      let completed = assessment.recording_url ?
+      let completed = assessment.recording_url ? (
         <div>
           <h6>Completed: Yes</h6>
-          <button className="button button-hover" onClick={this.downloadFile.bind(this,assessment.id)}>Get Download Link</button>
-        </div> :
+          <button
+            className="button button-hover"
+            onClick={this.downloadFile.bind(this, assessment.id)}
+          >
+            Get Download Link
+          </button>
+        </div>
+      ) : (
         <h6>Completed: No</h6>
-      let className = this.state.assessmentID === assessment.id ?
-        'selected' :
-        'playlist-card'
+      );
+      let className =
+        this.state.assessmentID === assessment.id
+          ? "selected"
+          : "playlist-card";
 
-      let dl = this.state.downloaded.includes(assessment.id) ? <button className="button button-hover" type="submit" value="Submit"><a href={this.state.downloaded[1]}>Download</a></button> : null
-
+      let dl = this.state.downloaded.includes(assessment.id) ? (
+        <button className="button button-hover" type="submit" value="Submit">
+          <a href={this.state.downloaded[1]}>Download</a>
+        </button>
+      ) : null;
+      let dv = this.state.downloaded.includes(assessment.id) ? (
+        <video src={this.state.downloaded[1]} autoPlay="true" controls="true" />
+      ) : null;
       return (
         <div
           key={i}
@@ -58,26 +74,24 @@ class AssessmentsDashboard extends Component {
             {assessment.patient_email.email}
           </h5>
           <h6>{assessment.playlist_name.name}</h6>
-          <h6>{assessment.date_added.slice(0,-6)}</h6>
+          <h6>{assessment.date_added.slice(0, -6)}</h6>
           {completed}
           {dl}
+          {dv}
         </div>
-      )
-    })
+      );
+    });
     return (
       <div>
         <div className="banner-text">
           <h1 className="banner-bold">Patient Assessments</h1>
         </div>
         <div className="content">
-          <div className="dash-nav">
-          </div>
-          <div className="playlist-container">
-            {assessments}
-          </div>
+          <div className="dash-nav" />
+          <div className="playlist-container">{assessments}</div>
         </div>
       </div>
-    )
+    );
   }
 }
 export default AssessmentsDashboard;

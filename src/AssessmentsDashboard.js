3ac747d4d0bed8e86d7bb3725577e4f9d6dfdfd3
toChaim <toChaim@gmail.com>
2017-10-02 13:06:48 -0700

    fix select for videos on assessments dashboard.

--- src/AssessmentsDashboard.js
+++ src/AssessmentsDashboard.js
@@ -7,89 +7,93 @@ class AssessmentsDashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      doctorAssessments: []
+      doctorAssessments: [],
+      assessmentID: null,
+      downloaded: []
     };
+  }
+
+  downloadFile(id) {
+    axios.get(`${BASE_URL}/api/recording/${id}`, config()).then(response => {
+      this.setState({ downloaded: [response.data.id, response.data.url] });
+    });
+  }
+
+  selectAssessment(assessmentID) {
+    if (
+      this.state.assessmentID === null ||
+      assessmentID !== this.state.assessmentID
+    ) {
+      this.setState({ assessmentID });
+    } else if (this.state.assessmentID !== null) {
+      this.setState({ assessmentID: null });
+    }
   }
 
   componentWillMount() {
     axios
       .get(`${BASE_URL}/api/users/${userID()}/assessments`, config())
       .then(response => {
-        Promise.all(
-          response.data.map(a => {
-            if (a.recording_url)
-              return axios.get(`${BASE_URL}/api/recording/${a.id}`, config());
-            else return null;
-          })
-        ).then(downloadLinks => {
-          let assessments = downloadLinks.map((v, i) => {
-            if (v)
-              return Object.assign(
-                { download_url: v.data.url },
-                response.data[i]
-              );
-            else return response.data[i];
-          });
-          this.setState({ doctorAssessments: assessments }, () => {});
-        });
+        this.setState({ doctorAssessments: response.data });
       });
   }
 
   render() {
     let assessments = this.state.doctorAssessments.map((assessment, i) => {
-      if (assessment.recording_url) {
-        return (
-          <div
-            key={i}
-            className={`playlist-card button-hover playlist-card-contents`}
-            onClick={e => {
-              e.currentTarget.className = e.currentTarget.className.includes(
-                "video-selected"
-              )
-                ? e.currentTarget.className.replace(" video-selected", "")
-                : e.currentTarget.className + " video-selected";
-            }}
-          >
-            <h5 className="playlist-name-title">
-              {assessment.patient_email.email}
-            </h5>
-            <h6>{assessment.playlist_name.name}</h6>
-            <h6>{assessment.date_added.slice(0, -6)}</h6>
+      console.log(
+        assessment.recording_url,
+        !!assessment.recording_url,
+        this.state.downloaded.includes(assessment.id)
+      );
+      let completed = <h6>Completed: No</h6>;
+      let className = "playlist-card";
+      if (this.state.downloaded.includes(assessment.id)) {
+        completed = (
+          <div>
             <div>
-              <div>
-                <video
-                  className="playlist-video"
-                  src={assessment.download_url}
-                  preload="metadata"
-                  controls="true"
-                />
-              </div>
-              <button
-                className="button button-hover"
-                type="submit"
-                value="Submit"
-              >
-                <a href={assessment.download_url}>Download</a>
-              </button>
+              <video
+                className="playlist-video"
+                src={this.state.downloaded[1]}
+                preload="metadata"
+                controls="true"
+              />
             </div>
+            <button
+              className="button button-hover"
+              type="submit"
+              value="Submit"
+            >
+              <a href={this.state.downloaded[1]}>Download</a>
+            </button>
           </div>
         );
-      } else {
-        return (
-          <div
-            key={i}
-            className={`playlist-card button-hover playlist-card-contents`}
+        className = "selected";
+      } else if (assessment.recording_url) {
+        completed = (
+          <button
+            className="button button-hover"
+            onClick={this.downloadFile.bind(this, assessment.id)}
           >
-            <h5 className="playlist-name-title">
-              {assessment.patient_email.email}
-            </h5>
-            <h6>{assessment.playlist_name.name}</h6>
-            <h6>{assessment.date_added.slice(0, -6)}</h6>
-            <h6>Completed: No</h6>
-          </div>
+            Get Video
+          </button>
         );
       }
+
+      return (
+        <div
+          key={i}
+          className={`${className} button-hover playlist-card-contents`}
+        >
+          <h5 className="playlist-name-title">
+            {assessment.patient_email.email}
+          </h5>
+          <h6>{assessment.playlist_name.name}</h6>
+          <h6>{assessment.date_added.slice(0, -6)}</h6>
+          {completed}
+        </div>
+      );
     });
+
     return (
       <div>
         <div className="banner-text">
